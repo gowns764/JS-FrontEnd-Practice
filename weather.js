cfec73ca90bde39f67e4c6dbc262bddfbc4317e0
Raspbian <gowns764@naver.com>
2020-12-17 22:21:38 +0900

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "f36bf82673ff109b9f484935de6be606";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -9,11 +23,13 @@ function handleGeoSucces(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coordsObj = {
+        // 키와 밸류 값이 같음을 의미
         latitude,
         longitude
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -30,7 +46,8 @@ function loadCoords() {
     if(loadedCords === null){
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
